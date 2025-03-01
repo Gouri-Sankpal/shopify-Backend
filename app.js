82473ec0bc2dabfe1639c8c834fcaa7be289569f
Gouri <gourisankpal3136@gmail.com>
2025-03-01 16:03:24 +0530

    task4 done

--- app.js
+++ app.js
@@ -6,41 +6,40 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const morgan = require('morgan');
+const {Product}= require('./model/Product');
 
 
-
-
-
-
-
-//Connecting to database
+//connecting to database
 mongoose.connect('mongodb://127.0.0.1:27017/shopifyEcom')
 .then(()=>{
     console.log('Connected to database');
 }).catch((err)=>{
-    console.log('database is not connected',err);
+    console.log('database is notConnected', err);
 })
+
 
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
-//task-1 create a register route
+
+//task-1 -> create a register route
 app.post('/register',async(req,res)=>{
     try{
 
-        const {name,email,password}= req.body;
-        //checking if any filed missing 
+        const {name,email,password} = req.body;
+        //check is any  field missing
         if(!name || !email || !password){
-            return res.status(400).json({message:'some fileds are Missing'});
-
+            return res.status(400).json({message:'Some fields are Missing'});
         }
-        //check if already exists
+
+        //check if user already exists
         const isUserAlreadyExists = await User.findOne({email});
         if(isUserAlreadyExists){
             return res.status(400).json({message:'User already exists'})
         }else{
+
             //hashing the password
             const salt = await bcrypt.genSaltSync(10);
             const hashedPassword = await bcrypt.hashSync(password,salt);
@@ -48,70 +47,104 @@ app.post('/register',async(req,res)=>{
             //jwt token
             const token = jwt.sign({email},'supersecret',{expiresIn:'365d'});
 
-            // creating new user
+            //creating new user
             await User.create({
                 name,
-                email,password:hashedPassword,
+                email,
+                password:hashedPassword,
                 token,
                 role:'user'
             })
-            return res.status(201).json({message:'user created sucessfully'})
-
+            return res.status(201).json({message:'User created successfully'});
         }
+        
 
     }catch(error){
-        conasole.log(error);
+        console.log(error);
         return res.status(500).json({message:'Internal server error'})
-
     }
 })
 
-//task-2 ->create a login route
+//task-2 -> create a login route
 app.post('/login',async(req,res)=>{
     try{
-        const {email,password}=req.body;
+        const {email, password} = req.body;
 
-    //check if any filed is missing
-    if(!email ||!password){
-        return res.status(400).json({message:'some fields are missing'})
-    }
+        //check if any field is missing
+        if(!email || !password){
+            return res.status(400).json({message:'Some fields are missing'});
+        }
+    
+        //user exists or not
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:'User does not exists.Please register first'});
+        }
 
-    //user exists or not 
-    const user = await User.findOne({email});
-    if(!user){
-        return res.status(400).json({message:'user does not exists.Please register first'});
-    }
+        //compare the entered password with the hashed password
+        const isPasswordMatched = await bcrypt.compareSync(password,user.password);
+        if(!isPasswordMatched){
+            return res.status(400).json({message:"Password is incorrect"});
+        }
 
-    //compaire the entered password with the hashed password
-    const isPasswordMatched= await bcrypt.compareSync(passwortd,user.password)
-    if(!isPasswordMatched){
-        return res.status(400).json({message:'Password is incorrect'});
-    }
-
-    //sucessfully loged in 
-    return res.status(200).json({
-    message:'user logged in successfully',
-    id:user._id,
-    name:user.name,
-    email:user.email.email,
-    token:user.token,
-    role:user.role
-    })
-}catch(error){
-        conasole.log(error);
+        //succesfully logged in
+        return res.status(200).json({
+            message:'user logged in successfully',
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            token:user.token,
+            role:user.role
+        })
+    }catch(error){
+        console.log(error);
         return res.status(500).json({message:'Internal server error'})
     }
- })
+})
 
+//task-3 -> create a route to see all products
+app.get('/products',async(req,res)=>{
+    try{
+       const products = await Product.find({});
+       return res.status(200).json({
+        message:"find all the products",
+        products:products
+       }) 
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
 
+//task-4 -> create va route to add a product
+app.post('/add-product',async(req,res)=>{
+    try{
+        const{name,price,image,brand,stock,description} = req.body;
+        const{token} = req.headers;
 
+        const decodedToken = jwt.verify(token,'supersecret');
+         const user = await User.findOne({email:decodedToken.email});
 
-
-
-
+         await Product.create({
+            name,
+            price,
+            description,
+            image,
+            stock,
+            brand,
+            user:user._id
+         })
+         return res.status(201).json({
+            message:'Product added successfully'
+         })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
 
 
 const PORT = 8080;
 app.listen(PORT,()=>{
-    console.log(`Server is connected to to port ${PORT}`);
+    console.log(`Server is connected to port ${PORT}`);
 })
